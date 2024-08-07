32097598014b6bfe485c82d4671fe10ee77e5891
ANAS <anaspappadan@gmail.com>
2024-08-07 11:49:58 +0530

    feat(backend) : Add product schema

---
+++ backend/schema/productSchema.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+
+const productSchema = new mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    description: { type: String },
+    price: { type: Number, required: true },
+    oldPrice: { type: Number },
+    image: { type: String, required: true },
+    category: { type: String, required: true },
+  },
+  { timestamps: true }
+);
+
+export default mongoose.model("Product", productSchema);
