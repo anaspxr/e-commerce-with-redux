5f7a4f9268148a8b3145b081c78287d951626058
ANAS <anaspappadan@gmail.com>
2024-08-08 11:15:05 +0530

    feat(backend): install bcrypt and secure password in authrouter

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,23 +1,24 @@
 import express from "express";
 import User from "../schema/userSchema.js";
+import bcrypt from "bcryptjs";
 
 const router = express.Router();
 
 ///api/users/register
-router.post("/register", (req, res) => {
-  const newUser = new User({
-    name: req.body.name,
-    email: req.body.email,
-    password: req.body.password,
-  });
-  newUser
-    .save()
-    .then(() => {
-      res.sendStatus(201);
-    })
-    .catch((error) => {
-      res.status(400).json({ message: error.message });
+router.post("/register", async (req, res) => {
+  try {
+    const hashedPassword = await bcrypt.hash(req.body.password, 10);
+    const newUser = new User({
+      name: req.body.name,
+      email: req.body.email,
+      password: hashedPassword,
     });
+
+    await newUser.save();
+    res.sendStatus(201);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
 });
 
 export default router;
