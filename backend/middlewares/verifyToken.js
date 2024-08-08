c3c5c40da0a3ca46ff9c47613ba4a80f164c2875
ANAS <anaspappadan@gmail.com>
2024-08-08 17:00:37 +0530

    feat(backend): create verify token middleware

---
+++ backend/middlewares/verifyToken.js
@@ -0,0 +1,27 @@
+import jwt from "jsonwebtoken";
+
+export const verifyToken = (req, res, next) => {
+  const authHeader = req.headers.token;
+  if (authHeader) {
+    const token = authHeader.split(" ")[1];
+    jwt.verify(token, process.env.JWT_SEC, (err, user) => {
+      if (err) res.status(403).json("Invalid Token");
+      else {
+        req.user = user;
+        next();
+      }
+    });
+  } else {
+    return res.status(401).json("You are not authenticated");
+  }
+};
+
+export const verifyTokenAndAuthZ = (req, res, next) => {
+  verifyToken(req, res, () => {
+    if (req.user.id === req.params.id || req.user.isAdmin) {
+      next();
+    } else {
+      res.status(403).json("Not Authorized..");
+    }
+  });
+};
