ba76a091486ee0de126ade1f33b2b250a00af71b
ANAS <anaspappadan@gmail.com>
2024-08-10 23:04:40 +0530

    feat(backend): more api routes for cart : quantity, remove, getall, getbyid

--- backend/routes/cartRoutes.js
+++ backend/routes/cartRoutes.js
@@ -1,19 +1,90 @@
 import express from "express";
 import Cart from "../schema/cartSchema.js";
-import { verifyToken } from "../middlewares/verifyToken.js";
+import {
+  verifyToken,
+  verifyTokenAndAdmin,
+} from "../middlewares/verifyToken.js";
 
 const router = express.Router();
 
-// create cart
-router.post("/", verifyToken, async (req, res) => {
+// add items to cart
+router.patch("/quantity", verifyToken, async (req, res) => {
   try {
-    const alreadyExist = await Cart.findOne({ userID: req.body.userID });
-    if (alreadyExist) {
-      res.status(400).send("Cart for this user already exists in the database");
+    const { productID, quantity } = req.body;
+
+    let cart = await Cart.findOne({ userID: req.user.id });
+
+    if (!cart) {
+      // if no cart exists for the user, create a new cart
+      cart = new Cart({
+        userID: req.user.id,
+        products: [{ productID, quantity }],
+      });
     } else {
-      const newCart = new Cart(req.body);
-      const savedCart = await newCart.save();
-      res.status(200).json(savedCart);
+      // checking if the product is already in the cart
+      const productIndex = cart.products.findIndex(
+        (p) => p.productID.toString() === productID
+      );
+
+      if (productIndex > -1) {
+        // product exists in the cart, update the quantity
+        cart.products[productIndex].quantity += quantity;
+      } else {
+        // product doesn't exist in the cart, add it
+        cart.products.push({ productID, quantity });
+      }
+    }
+
+    // Save the cart
+    const savedCart = await cart.save();
+    res.status(200).json(savedCart);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+router.patch("/remove", verifyToken, async (req, res) => {
+  try {
+    const cart = await Cart.findOneAndUpdate(
+      { userID: req.user.id, "products.productID": req.body.productID },
+      {
+        $pull: { products: { productID: req.body.productID } }, // if product exists, delete
+      },
+      { new: true }
+    );
+    if (cart) {
+      res.status(200).json(cart);
+    } else {
+      res.status(404).json({ message: "Product not found" });
+    }
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+//admin
+// get all cart
+router.get("/", verifyTokenAndAdmin, async (req, res) => {
+  try {
+    const data = await Cart.find();
+    if (data) {
+      res.status(200).json(data);
+    } else {
+      res.status(400).json({ message: "something went wrong" });
+    }
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+// get cart of individual users
+router.get("/:id", verifyTokenAndAdmin, async (req, res) => {
+  try {
+    const data = await Cart.findOne({ userID: req.params.id });
+    if (data) {
+      res.status(200).json(data);
+    } else {
+      res.status(404).json({ message: "No cart found for this user" });
     }
   } catch (error) {
     res.status(500).json(error);
