5f8184ae1a5884cc4358fa90bbbd7d024e9f1550
ANAS <anaspappadan@gmail.com>
2024-08-10 18:12:48 +0530

    perf(backend): create model for cart
    instead of using cart as embedded document inside user model, its better to have its own model since cart will get updated multiple times

---
+++ backend/schema/cartSchema.js
@@ -0,0 +1,27 @@
+import mongoose from "mongoose";
+const cartSchema = new mongoose.Schema(
+  {
+    userID: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    products: [
+      {
+        productID: {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: "Product",
+          required: true,
+        },
+        quantity: {
+          type: Number,
+          required: true,
+          min: 1,
+        },
+      },
+    ],
+  },
+  { timestamps: true }
+);
+
+export default mongoose.model("Cart", cartSchema);
