2fe839f22db07bde5bcd0f9f82fe200f718b3004
ANAS <anaspappadan@gmail.com>
2024-08-12 12:02:25 +0530

    refactor(backend): moving controller functions to controllers folder - user

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -3,62 +3,26 @@ import {
   verifyTokenAndAdmin,
   verifyTokenAndAuthZ,
 } from "../middlewares/verifyToken.js";
-import bcrypt from "bcryptjs";
-import User from "../schema/userSchema.js";
+import {
+  getAllUsers,
+  getUser,
+  updateUser,
+  deleteUser,
+} from "../controllers/userController.js";
 
 const router = express.Router();
-// url/api/users
 
 // get all users
-router.get("/", verifyTokenAndAdmin, async (req, res) => {
-  try {
-    const users = await User.find();
-    res.status(200).json({ users });
-  } catch (error) {
-    res.status(500).json(error);
-  }
-});
+router.get("/", verifyTokenAndAdmin, getAllUsers);
 
 //get individual user
-router.get("/:id", verifyTokenAndAdmin, async (req, res) => {
-  try {
-    const user = await User.findById(req.params.id);
-    res.status(200).json({ user });
-  } catch (error) {
-    res.status(500).json(error);
-  }
-});
+router.get("/:id", verifyTokenAndAdmin, getUser);
 
 // update one user
-router.put("/:id", verifyTokenAndAuthZ, async (req, res) => {
-  try {
-    if (req.body.password) {
-      const saltRounds = parseInt(process.env.SALT_ROUNDS, 10);
-      const hashedPassword = await bcrypt.hash(req.body.password, saltRounds);
-      req.body.password = hashedPassword;
-    }
-    const updatedUser = await User.findByIdAndUpdate(
-      req.params.id,
-      {
-        $set: req.body,
-      },
-      { new: true }
-    );
-    res.status(200).json(updatedUser);
-  } catch (error) {
-    res.status(500).send(error.message);
-  }
-});
+router.put("/:id", verifyTokenAndAuthZ, updateUser);
 
 //  delete one user
-router.delete("/:id", verifyTokenAndAdmin, async (req, res) => {
-  try {
-    await User.findByIdAndDelete(req.params.id);
-    res.status(200).send("User has been deleted");
-  } catch (error) {
-    res.status(500).json(error);
-  }
-});
+router.delete("/:id", verifyTokenAndAdmin, deleteUser);
 
 // todo: get user stats  url/api/users/stats
 
