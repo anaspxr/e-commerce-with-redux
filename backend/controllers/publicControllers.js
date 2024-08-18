23e5bdd22ca84e36bb7809b968a0d4aa60a72822
ANAS <anaspappadan@gmail.com>
2024-08-18 21:25:24 +0530

    refactor(backend): Update public controllers to use CustomError class and tryCatch function

--- backend/controllers/publicControllers.js
+++ backend/controllers/publicControllers.js
@@ -1,35 +1,33 @@
 import Product from "../schema/productSchema.js";
+import CustomError from "../utils/CustomError.js";
+import tryCatch from "../utils/trycatch.js";
 
-const getProducts = async (req, res) => {
-  try {
-    const queryNew = req.query.new;
-    const queryCategory = req.query.category;
-    let products;
-    if (queryNew) {
-      if (queryCategory)
-        products = await Product.find({ category: { $in: [queryCategory] } })
-          .sort({ createdAt: -1 })
-          .limit(5);
-      else products = await Product.find().sort({ createdAt: -1 }).limit(5);
-    } else if (queryCategory) {
-      products = await Product.find({ category: { $in: [queryCategory] } });
-    } else {
-      products = await Product.find();
-    }
-    res.status(200).json(products);
-  } catch (error) {
-    res.status(500).json(error);
+const getProducts = tryCatch(async (req, res) => {
+  const queryNew = req.query.new;
+  const queryCategory = req.query.category;
+  let products;
+  if (queryNew) {
+    if (queryCategory)
+      // if both new and category query parameters are provided
+      products = await Product.find({ category: { $in: [queryCategory] } })
+        .sort({ createdAt: -1 })
+        .limit(5);
+    else products = await Product.find().sort({ createdAt: -1 }).limit(5); // if only new query parameter is provided
+  } else if (queryCategory) {
+    // if only category query parameter is provided
+    products = await Product.find({ category: { $in: [queryCategory] } });
+  } else {
+    // if no query parameters are provided return all products
+    products = await Product.find();
   }
-};
+  res.status(200).json(products);
+});
 
-const getProductById = async (req, res) => {
-  try {
-    const id = req.params.id;
-    const product = await Product.findById(id);
-    res.status(200).json({ product });
-  } catch (error) {
-    res.status(500).json(error);
-  }
-};
+const getProductById = tryCatch(async (req, res, next) => {
+  const id = req.params.id;
+  const product = await Product.findById(id);
+  if (!product) return next(new CustomError("Product not found", 404));
+  res.status(200).json({ product });
+});
 
 export { getProducts, getProductById };
