1e85c912d75b1cbc18251c7a17240f5a2b122826
ANAS <anaspappadan@gmail.com>
2024-08-21 15:45:39 +0530

    refactor(frontend): Update userSlice to use createAsyncThunk for login,logout,refreshToken actions

--- frontend/src/Store/userSlice.js
+++ frontend/src/Store/userSlice.js
@@ -1,9 +1,13 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import axios from "axios";
+import axiosErrorCatch from "../utils/axiosErrorCatch";
 
 const initialState = {
   currentUser: null,
   isAdmin: false,
   accessToken: null,
+  loading: false,
+  error: null,
   redirectPath: "/",
 };
 
@@ -11,23 +15,96 @@ const userSlice = createSlice({
   name: "user",
   initialState,
   reducers: {
-    login: (state, action) => {
-      state.accessToken = action.payload.accessToken;
-      state.currentUser = action.payload.user;
-      state.isAdmin = action.payload.user.isAdmin;
-    },
-    logout: (state) => {
-      // clear the accessToken and reset all states
-      state.accessToken = null;
-      state.currentUser = null;
-      state.isAdmin = false;
-    },
     setRedirectPath: (state, action) => {
       state.redirectPath = action.payload;
     },
   },
+  extraReducers: (builder) => {
+    builder //login
+      .addCase(login.pending, (state) => {
+        state.loading = true;
+        state.error = null;
+      })
+      .addCase(login.fulfilled, (state, action) => {
+        state.loading = false;
+        state.accessToken = action.payload.accessToken;
+        state.currentUser = action.payload.user;
+        state.isAdmin = action.payload.user.isAdmin;
+      })
+      .addCase(login.rejected, (state, action) => {
+        state.loading = false;
+        state.error = action.error.message;
+      });
+    builder // logout
+      .addCase(logout.pending, (state) => {
+        state.loading = true;
+        state.error = null;
+      })
+      .addCase(logout.fulfilled, (state) => {
+        state.loading = false;
+        state.accessToken = null;
+        state.currentUser = null;
+        state.isAdmin = false;
+      })
+      .addCase(logout.rejected, (state, action) => {
+        state.loading = false;
+        state.error = action.error.message;
+      });
+    builder //refreshToken
+      .addCase(refreshToken.fulfilled, (state, action) => {
+        state.currentUser = action.payload.user;
+        state.accessToken = action.payload.accessToken;
+        state.isAdmin = action.payload.user.isAdmin;
+      });
+  },
 });
 
-export const { login, logout, setRedirectPath } = userSlice.actions;
+export const login = createAsyncThunk(
+  "user/login",
+  async (values, { rejectWithValue }) => {
+    try {
+      const { data } = await axios.post("http://localhost:3000/api/auth/login", values, {
+        withCredentials: true,
+      });
+      return data; //data contains user (user details) and accessToken
+    } catch (error) {
+      return rejectWithValue(axiosErrorCatch(error));
+    }
+  }
+);
+
+export const logout = createAsyncThunk("user/logout", async (_, { rejectWithValue }) => {
+  try {
+    return await axios.post(
+      "http://localhost:3000/api/auth/logout",
+      {},
+      {
+        withCredentials: true,
+      }
+    );
+  } catch (error) {
+    return rejectWithValue(axiosErrorCatch(error));
+  }
+});
+
+export const refreshToken = createAsyncThunk(
+  "user/refreshToken",
+  async (_, { rejectWithValue }) => {
+    try {
+      const response = await axios.post(
+        "http://localhost:3000/api/auth/refreshtoken",
+        {},
+        {
+          withCredentials: true, // Ensures cookies are included in the request
+        }
+      );
+      return { user: response.data.user, accessToken: response.data.accessToken };
+    } catch (error) {
+      return rejectWithValue(console.log(axiosErrorCatch(error)));
+    }
+  }
+);
+
+export const { setRedirectPath } = userSlice.actions;
 
 export default userSlice.reducer;
