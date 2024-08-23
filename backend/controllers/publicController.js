cc0a87a0313de831b73695543e0f1cdb8321b7c4
ANAS <anaspappadan@gmail.com>
2024-08-23 12:48:18 +0530

    feat(backend): Optimize getProducts function for improved performance and pagination

--- backend/controllers/publicController.js
+++ backend/controllers/publicController.js
@@ -2,25 +2,68 @@ import Product from "../schema/productSchema.js";
 import Review from "../schema/reviewSchema.js";
 import CustomError from "../utils/CustomError.js";
 
+// const getProducts = async (req, res) => {
+//   const queryNew = req.query.new;
+//   const queryCategory = req.query.category;
+//   let products;
+//   if (queryNew) {
+//     if (queryCategory)
+//       // if both new and category query parameters are provided
+//       products = await Product.find({ category: { $in: [queryCategory] } })
+//         .sort({ createdAt: -1 })
+//         .limit(5);
+//     else products = await Product.find().sort({ createdAt: -1 }).limit(5); // if only new query parameter is provided
+//   } else if (queryCategory) {
+//     // if only category query parameter is provided
+//     products = await Product.find({ category: { $in: [queryCategory] } });
+//   } else {
+//     // if no query parameters are provided return all products
+//     products = await Product.find();
+//   }
+//   res.status(200).json(products);
+// };
+
 const getProducts = async (req, res) => {
-  const queryNew = req.query.new;
-  const queryCategory = req.query.category;
-  let products;
-  if (queryNew) {
-    if (queryCategory)
-      // if both new and category query parameters are provided
-      products = await Product.find({ category: { $in: [queryCategory] } })
-        .sort({ createdAt: -1 })
-        .limit(5);
-    else products = await Product.find().sort({ createdAt: -1 }).limit(5); // if only new query parameter is provided
-  } else if (queryCategory) {
-    // if only category query parameter is provided
-    products = await Product.find({ category: { $in: [queryCategory] } });
-  } else {
-    // if no query parameters are provided return all products
-    products = await Product.find();
+  //? gets products based on query parameters: new, category, page, limit
+
+  const { new: queryNew, category: queryCategory, page, limit } = req.query;
+  const pageNum = parseInt(page) || 1;
+  const limitNum = parseInt(limit) || 10;
+  const skip = (pageNum - 1) * limitNum;
+
+  let matchStage = {};
+
+  if (queryCategory) {
+    // if category query parameter is provided, include it in the match stage
+    matchStage.category = { $in: [queryCategory] };
   }
-  res.status(200).json(products);
+
+  const aggregationPipeline = [{ $match: matchStage }]; // initial stage of aggregation pipeline
+
+  if (queryNew) {
+    // if new query parameter is provided, sort by createdAt field and limit to 5
+    aggregationPipeline.push(
+      { $sort: { createdAt: -1 } },
+      { $limit: limitNum }
+    );
+  }
+
+  if (page) {
+    // if pagination is requested, push skip and limit stages to aggregation pipeline
+    aggregationPipeline.push({ $skip: skip }, { $limit: limitNum });
+  }
+
+  // if no pagination is requested, fetch 100 products
+  if (!page && !queryNew) {
+    aggregationPipeline.push({ $limit: 100 });
+  }
+
+  const products = await Product.aggregate(aggregationPipeline);
+
+  // calculate total documents for pagination metadata
+  const total = await Product.countDocuments(matchStage);
+
+  res.status(200).json({ products, page: pageNum, total });
 };
 
 const getProductById = async (req, res, next) => {
@@ -31,10 +74,9 @@ const getProductById = async (req, res, next) => {
 };
 
 const getReviewsOfProduct = async (req, res, next) => {
-  const reviews = await Review.find({ productID: req.params.productID }).populate(
-    "userID",
-    "name email"
-  );
+  const reviews = await Review.find({
+    productID: req.params.productID,
+  }).populate("userID", "name email");
   if (!reviews) return next(new CustomError("Reviews not found", 404));
   res.status(200).json(reviews);
 };
