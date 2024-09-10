205fa7279e99b71a012042b04f3175db7a7c0ee2
ANAS <anaspappadan@gmail.com>
2024-09-10 11:18:43 +0530

    refactor(admin): update users list to table view

--- frontend/src/Admin/components/UsersList.jsx
+++ frontend/src/Admin/components/UsersList.jsx
@@ -1,8 +1,9 @@
 import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
-export default function UsersList({ users, setSelectedUser }) {
+export default function UsersList({ users }) {
   const [toggle, setToggle] = useState(false);
-
+  const navigate = useNavigate();
   return (
     <div>
       <div className="flex justify-end items-center gap-2">
@@ -12,34 +13,46 @@ export default function UsersList({ users, setSelectedUser }) {
           {toggle ? "Show Users" : "Show Admins"}
         </button>
       </div>
-      <ul className="flex flex-col justify-between space-y-2">
-        {users.length === 0 && <p>No results found</p>}
-        {users
-          .filter((user) => (toggle ? user.isAdmin : !user.isAdmin))
-          .map((user) => (
-            <li
-              key={user._id}
-              className="flex justify-between sm p-2 bg-gray-200 rounded-md">
-              <div>
-                <p className="text-slate-500">{user.name}</p>
-                <p className="text-slate-700">{user.email}</p>
-                {/* {!toggle && (
-                  <p className="text-slate-600">Orders:{user.orders?.length}</p>
-                )} */}
-              </div>
-              {!toggle && (
-                <button
-                  className="text-blue-500 w-40 p-1 rounded-md hover:bg-slate-300"
-                  onClick={() => {
-                    window.scrollTo(0, 0);
-                    setSelectedUser(user);
-                  }}>
-                  See Details
-                </button>
-              )}
-            </li>
-          ))}
-      </ul>
+      <table className="min-w-full border-collapse border-2 bg-white">
+        <thead>
+          <tr>
+            <th className="p-2 text-left text-slate-500">Index</th>
+            <th className="p-2 text-left text-slate-500">Name</th>
+            <th className="p-2 text-left text-slate-500">Email</th>
+            {!toggle && <th className="p-2 text-left text-slate-500"></th>}
+          </tr>
+        </thead>
+        <tbody>
+          {users.length === 0 && (
+            <tr>
+              <td colSpan={3} className="p-2 text-center text-slate-500">
+                No results found
+              </td>
+            </tr>
+          )}
+          {users
+            .filter((user) => (toggle ? user.isAdmin : !user.isAdmin))
+            .map((user, i) => (
+              <tr key={user._id} className="border ">
+                <td className="p-2 py-4 text-slate-700">{i + 1}</td>
+                <td className="p-2 text-slate-700">{user.name}</td>
+                <td className="p-2 text-slate-700">{user.email}</td>
+                {!toggle && (
+                  <td className="p-2">
+                    <button
+                      className="text-blue-500 w-40 p-1 rounded-md hover:bg-slate-300"
+                      onClick={() => {
+                        window.scrollTo(0, 0);
+                        navigate(`/admin/users/${user._id}`);
+                      }}>
+                      See Details
+                    </button>
+                  </td>
+                )}
+              </tr>
+            ))}
+        </tbody>
+      </table>
     </div>
   );
 }
--- frontend/src/Admin/pages/UsersPage.jsx
+++ frontend/src/Admin/pages/UsersPage.jsx
@@ -1,8 +1,5 @@
-import { useState } from "react";
-
 import useGetPrivateData from "../../hooks/useGetPrivateData.js";
 import UsersList from "../components/UsersList.jsx";
-import UserDetails from "../components/UserDetails.jsx";
 
 export default function UsersPage() {
   const {
@@ -11,17 +8,12 @@ export default function UsersPage() {
     error: errorUsers,
   } = useGetPrivateData("admin/users");
 
-  const [selectedUser, setSelectedUser] = useState(null);
-
   return (
     <div>
       <h1 className="text-2xl font-semibold text-slate-700">Users</h1>
-      {selectedUser && <UserDetails user={selectedUser} />}
       {errorUsers && <p>{errorUsers.message}</p>}
       {loadingUsers && <p>Loading...</p>}
-      {data && (
-        <UsersList users={data.users} setSelectedUser={setSelectedUser} />
-      )}
+      {data && <UsersList users={data.users} />}
     </div>
   );
 }
